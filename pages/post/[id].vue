14d917fd8377aaa6a9c20dd83912da6740c923af
takashi145 <t.shuhei0203@gmail.com>
2023-02-23 22:48:27 +0900

    delete post

--- pages/post/[id].vue
+++ pages/post/[id].vue
@@ -8,11 +8,23 @@ const config = useRuntimeConfig();
 const route = useRoute();
 
 const { data: post } = await useFetch(`/api/post/${route.params.id}`);
+
+const deletePost = async () => {
+  try {
+    await $fetch(`/api/post/${route.params.id}`, {
+      method: "DELETE"
+    });
+    return navigateTo('/post');
+  }catch(e) {
+    console.log(e.response);
+  }
+}
 </script>
 <template>
   <div>
     <img :src="`${config.public.supabase.url}/storage/v1/object/public/images/${post.image}`" class="border-2" style="width: 300px;">
     <h3>{{ post.title }}</h3>
     <p>{{ post.description }}</p>
+    <button @click="deletePost" class="text-red-400">delete</button>
   </div>
 </template>
